84d6c3f15a4fba3e6512fe80cc914c02ff35e361
rmdocherty <rmdocherty@users.noreply.github.com>
2020-10-01 13:17:10 +0100

    Added code to visualise sample data
    read_data opens the sample cluster data and has implementation of delta encoding

---
+++ satellite/test/read_data.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  1 12:05:08 2020
+
+@author: Ronan
+"""
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+
+DATA_PATH = "C:\\Users\\Ronan\\Documents\\uni_work\\physics\\third year\\project\\data\\test_data\\C1_160308.txt"
+
+def load_data(path):
+    raw_data = np.loadtxt(path).T #724662 entries
+    return raw_data
+
+def fix_data(data):
+    fixed_x = data[1] * 10000
+    fixed_y = data[2] * 10000
+    fixed_z = data[3] * 10000
+    fixed_array = np.array([fixed_x, fixed_y, fixed_z], dtype = np.int32)
+    return fixed_array
+
+def delta_data(buffer_length, data_list):
+    buffer_regions = [i*buffer_length for i in range(0, len(data_list)//buffer_length)]
+    compressed = []
+    for buffer_start in buffer_regions:
+        ref_point = data_list[buffer_start]
+        compressed.append(ref_point)
+        adjusted_list = []
+        for i in range(1, buffer_length):
+            delta = data_list[buffer_start + i] - ref_point
+            adjusted_list.append(delta)
+        #print(adjusted_list)
+        compressed.append(adjusted_list)
+    return compressed
+            
+            
+
+
+data = load_data(DATA_PATH)
+fixed_data = fix_data(data)
+print(delta_data(100, fixed_data[0])[0:10])
+
+
+#plt.plot(raw_data[0], raw_data[1], label = "x-data")
+#plt.plot(raw_data[0], raw_data[2], label = "y-data")
+#plt.plot(raw_data[0], raw_data[3], label = "z-data")
+
+#plt.xlabel("Time")
+#plt.ylabel("Magnetic field strength (nT)")
+#plt.legend()
