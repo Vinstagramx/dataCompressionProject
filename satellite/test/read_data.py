2ef26293d562fcce9962165d800e3e7ecb3fb0cf
rmdocherty <rmdocherty@users.noreply.github.com>
2020-10-02 10:51:13 +0100

    added golomb encoding
    added golomb encoding function and golomb function to perform the encoding as described in paper. Still need to add Huffman encoding to the remainders for full agreement with paper

--- satellite/test/read_data.py
+++ satellite/test/read_data.py
@@ -8,6 +8,7 @@ import pandas
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 DATA_PATH = "C:\\Users\\Ronan\\Documents\\uni_work\\physics\\third year\\project\\data\\test_data\\C1_160308.txt"
 
 def load_data(path):
@@ -42,13 +43,33 @@ def delta_data(buffer_length, data_list, squared = False):
             compressed.append(deltas)
     return compressed
             
-            
+def golomb_encoding(n,b):
+    q = n//b
+    r = n - q*b
+    quot = ["1" for i in range(0,q)] + ["0"]
+    return ("".join(quot), r)
 
+def golomb(buffer_length, data_list, rice = False):
+    buffer_regions = [i*buffer_length for i in range(0, len(data_list)//buffer_length)]
+    compressed = []
+    for buffer_start in buffer_regions:
+        data = data_list[buffer_start:(buffer_start+buffer_length)]
+        b = int(np.mean(data))
+        golombs = [golomb_encoding(i, b) for i in data]
+        remainders = [bin(i[1]) for i in golombs]
+        unaries = [i[0] for i in golombs]
+        compressed.append([(unaries[i], remainders[i]) for i in range(0, buffer_length)])
+    return compressed
+    
+       
 
+#use bin() function to get binary equivalent - then use len ti find out # of bits
+#any reason deltas + golomb can't be used?
+#need to implement binary/huffman encoding for remainder of golob
 data = load_data(DATA_PATH)
 fixed_data = fix_data(data)
-print(delta_data(100, fixed_data[0], squared=False)[-1])
-
+#print(delta_data(100, fixed_data[0], squared=False)[-1])
+print(golomb(100, fixed_data[0])[:4])
 
 #plt.plot(raw_data[0], raw_data[1], label = "x-data")
 #plt.plot(raw_data[0], raw_data[2], label = "y-data")
