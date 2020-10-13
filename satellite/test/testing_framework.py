4ebe44e16785e54d5fdeed6916cbdd06564d5436
rmdocherty <rmdocherty@users.noreply.github.com>
2020-10-13 12:05:47 +0100

    fixed testing framework

--- satellite/test/testing_framework.py
+++ satellite/test/testing_framework.py
@@ -14,28 +14,31 @@ import pandas as pd
 #%% 
 data_path = "C:\\Users\\Ronan\\Documents\\github\\Physics2020\\satellite\\data"
 csv = pd.read_csv("C:\\Users\\Ronan\\Documents\\github\\Physics2020\\satellite\\test\\stats.csv")
-loop = 0; max_ratios = []; max_blocks = []
+loop = 0
+max_ratios = []
+max_blocks = []
 
 for filename in os.listdir(data_path):
     file_index = filename[:9]
     dirs = ["x", "y", "z"]
     for direction in dirs:
         ratios = []
-        print(file_index, loop)
-        for i in range(2, 3, 1):
+        #print(file_index, loop)
+        for i in range(2, 4, 1):
             temp_encoder = Delta(data_path+"\\"+filename, i, 2, direction=direction)
             temp_encoder.encode_data(stats=False)
             ratios.append(temp_encoder.get_compression_ratio())
         max_ratio = max(ratios); block_size = ratios.index(max_ratio) + 2
+        #print(max_ratio)
         max_ratios.append(max_ratio)
-        max_blocks.append(max_blocks)
+        max_blocks.append(block_size)
         loop += 1
-print(max_ratio)
-print(max_blocks)
+        print(max_ratios)
+        print(max_blocks)
 #%%
 csv["Delta"] = max_ratios
 csv["Block"] = max_blocks
-csv.to_csv("stats_prime")
+csv.to_csv("stats_prime.csv")
         
     
     
