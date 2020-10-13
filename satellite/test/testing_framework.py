96245c173cba1817c0cb4cdc89ec21223e2b94bd
rmdocherty <rmdocherty@users.noreply.github.com>
2020-10-13 22:52:23 +0100

    combined the stats into stats_prime
    added code to testing framework to combine my half and your half into stats prime

--- satellite/test/testing_framework.py
+++ satellite/test/testing_framework.py
@@ -28,7 +28,7 @@ for filename in files[:6]:
     for direction in dirs:
         ratios = []
         print(file_index, loop)
-        for i in range(2, 100, 1):
+        for i in range(2, 100 , 1):
             temp_encoder = Delta(file_path, i, "all", direction=direction)
             temp_encoder.encode_data(stats=False)
             ratios.append(temp_encoder.get_compression_ratio())
@@ -43,6 +43,16 @@ np.save("max_blocks_to_18", max_blocks)
 #csv["Delta"] = max_ratios
 #csv["Block"] = max_blocks
 #csv.to_csv("stats_prime.csv")
+#%%
+up_to_18_ratios = list(np.load("max_ratios_to_18.npy"))
+up_to_18_blocks = list(np.load("max_blocks_to_18.npy"))
+from_19_csv = pd.read_csv("delta_compression_stats.csv")
+from_19_ratios = list(from_19_csv["Max Compression Ratio (Delta)"])
+from_19_blocks = list(from_19_csv["Block Size (Delta)"])
+ratio_data = up_to_18_ratios + from_19_ratios
+block_data = up_to_18_blocks + from_19_blocks
 
-    
-    
+csv["Delta"] = ratio_data
+csv["Block"] = block_data
+csv.to_csv("stats_prime.csv")
+
