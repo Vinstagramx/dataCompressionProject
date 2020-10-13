c755bbf6038a14448342fe4538199e9bdaa7e557
Vinstagramx <44545069+Vinstagramx@users.noreply.github.com>
2020-10-13 11:53:18 +0100

    changed the file path on testing-framework, fixed to work with os and github
    -  feel free to change the file path back

--- satellite/test/testing_framework.py
+++ satellite/test/testing_framework.py
@@ -11,19 +11,23 @@ import numpy as np
 import os
 import pandas as pd
 
+cwd = os.getcwd()
+data_folder = os.path.join(cwd, 'data')
+stats_file = os.path.join(cwd, 'test', 'stats.csv')
 #%% 
-data_path = "C:\\Users\\Ronan\\Documents\\github\\Physics2020\\satellite\\data"
-csv = pd.read_csv("C:\\Users\\Ronan\\Documents\\github\\Physics2020\\satellite\\test\\stats.csv")
+data_path = data_folder
+csv = pd.read_csv(stats_file)
 loop = 0; max_ratios = []; max_blocks = []
 
 for filename in os.listdir(data_path):
     file_index = filename[:9]
     dirs = ["x", "y", "z"]
+    file_path = os.path.join(data_path, filename)
     for direction in dirs:
         ratios = []
         print(file_index, loop)
         for i in range(2, 3, 1):
-            temp_encoder = Delta(data_path+"\\"+filename, i, 2, direction=direction)
+            temp_encoder = Delta(file_path, i, 2, direction=direction)
             temp_encoder.encode_data(stats=False)
             ratios.append(temp_encoder.get_compression_ratio())
         max_ratio = max(ratios); block_size = ratios.index(max_ratio) + 2
