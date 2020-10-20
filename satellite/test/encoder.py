7a95db5d582512c47ed0580054aa9147bd79d217
Vinstagramx <44545069+Vinstagramx@users.noreply.github.com>
2020-10-20 12:22:38 +0100

    manual b seter (percentage of max)

--- satellite/test/encoder.py
+++ satellite/test/encoder.py
@@ -230,6 +230,8 @@ class Golomb(Encoder):
             b = max(block)
         elif self._mode == "min":
             b = min(block)
+        elif isinstance(self._mode, int):
+            b = (self._mode/100) * max(block)
         golombs = [self.golomb(i, b) for i in block]
         data = np.concatenate(([self.binary(i[1]) for i in golombs], [i[0] for i in golombs] ), axis=None)
         return [[b], data]
