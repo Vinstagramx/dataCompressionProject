5fd5cd1efd2440e1c74a54e91c423da9e5f09b1c
Vinstagramx <vincent@siunits.co.uk>
2020-10-29 08:56:30 +0000

    Updated os plotter - added delta graphs to delta folder

--- satellite/test/plotter_os.py
+++ satellite/test/plotter_os.py
@@ -4,13 +4,16 @@ Created on Tue Oct  6 16:39:32 2020
 @author: Ronan
 Altered to use os package and to work with current working directories.
 """
-
+import matplotlib as mpl
 import os
 import matplotlib.pyplot as plt
 from encoder import Delta, Golomb, DeltaGR
 import numpy as np
 import pandas as pd
 
+'''
+'''
+mpl.rcParams['agg.path.chunksize'] = 10000
 cwd = os.getcwd()
 
 '''
@@ -18,9 +21,10 @@ Working with a singular file
 '''
 
 parent_dir = os.path.dirname(cwd)
+cwd = os.path.dirname(cwd)
 datafile = 'C3_160313.FS.FULLRES.txt'
 DATA_PATH = os.path.join(cwd, 'data', datafile)
-plot_path = os.path.join(cwd, 'test', 'plots')
+plot_path = os.path.join(cwd, 'test', 'plots', 'deltas')
 plt.clf()
 figure = plt.gcf()  # get current figure
 figure.set_size_inches(18, 10)
@@ -33,9 +37,9 @@ def plot_settings(datalength, filename):
         plt.xlim(0, datalength)
     plt.xticks(fontsize=18)
     plt.yticks(fontsize=18)
-    plt.title(f"Space Saving Ratio vs Block (buffer) size - {filename}", fontsize = 24)
-    plt.ylabel("Space Saving Ratio (%)", fontsize = 22)
-    plt.xlabel("Block size", fontsize = 22)
+    plt.title(f"Plot of Differences between each datapoint (deltas) - {filename}", fontsize = 24)
+    plt.ylabel("Difference in Magnetic Field Strength (nT)", fontsize = 22)
+    plt.xlabel("Time", fontsize = 22)
     
     
 def pretty_graph(x_label, y_label, title, fontsize): #formatting graphs
@@ -92,42 +96,48 @@ plt.plot(range(2, maxblocksize, step), yratios, label = 'y-direction')
 plt.plot(range(2, maxblocksize, step), zratios, label = 'z-direction')
 plot_settings(None, filename)
 plt.savefig(f'{plot_path}/spacesaving_ratio_xyz_{maxblocksize}_{filename}.png', dpi = 200)
+#%%
+"""
+Code to print raw data for multiple files
+"""
+data_folder = os.path.join(cwd, 'data')
+path_list = []
+filenames = []
+for file in os.listdir(data_folder):
+    if file.endswith(".txt"):
+        filenames.append(file.split('.')[0])
+        path_list.append(os.path.join(data_folder, file))
 
-# """
-# Code to print raw data for multiple files
-# """
-# data_folder = os.path.join(cwd, 'data')
-# path_list = []
-# filenames = []
-# for file in os.listdir(data_folder):
-#     if file.endswith(".txt"):
-#         filenames.append(file.split('.')[0])
-#         path_list.append(os.path.join(data_folder, file))
+file_dir_list = []
+std_list = []
+#rawdata_plotpath = os.path.join(osplot_path, 'data')
+#csv_path = os.path.join(cwd, 'test')
+for ind, path in enumerate(path_list):
+    d = Delta(path, 20, 1000, direction="x")
+    d.filter_data()
+    time_interval = 0.14866719 #time interval between every reading (in seconds) - FIXED
+    raw_data = d._data
+    dirs = ["x-direction", "y-direction", "z-direction"]
+    coord = ['x', 'y', 'z']
+    for index, i  in enumerate(raw_data):
+        figure = plt.gcf()  # get current figure
+        figure.set_size_inches(18, 10)
+        i = [value for value in i if abs(value) < (2**12) / 7.8125e-3]
+        compressed = []
+        for j in range(1,len(i)):
+            compressed.append(i[j]-i[j-1])
+        std = np.std(i)
+        print(f"{dirs[index]} standard dev is: {std}nT")
+        timeseries = np.asarray(range(len(i))) * time_interval
+        maxval = max(timeseries)
+        file_dir_list.append(f'{filenames[ind]} {coord[index]}')
+        std_list.append(std)
+        plt.plot(timeseries[1:], compressed, label = dirs[index])
 
-# file_dir_list = []
-# std_list = []
-# rawdata_plotpath = os.path.join(plot_path, 'raw_data')
-# csv_path = os.path.join(cwd, 'test')
-# for ind, path in enumerate(path_list):
-#     d = Delta(path, 20, 1000, direction="x")
-#     time_interval = 0.14866719 #time interval between every reading (in seconds) - FIXED
-#     raw_data = d._data
-#     dirs = ["x-direction", "y-direction", "z-direction"]
-#     coord = ['x', 'y', 'z']
-#     for index, i  in enumerate(raw_data):
-#         i = i / 10000
-#         std = np.std(i)
-#         print(f"{dirs[index]} standard dev is: {std}nT")
-#         timeseries = np.asarray(range(len(i))) * time_interval
-#         maxval = max(timeseries)
-#         file_dir_list.append(f'{filenames[ind]} {coord[index]}')
-#         std_list.append(std)
-#         # plt.plot(timeseries, i, label = dirs[index])
-
-#     # plot_settings(maxval, filenames[ind])   
-#     # plt.savefig(f'{rawdata_plotpath}/raw_data_{filenames[ind]}.png', dpi = 200)
-#     # plt.show() 
-#     # plt.clf()
+    plot_settings(maxval, filenames[ind])   
+    plt.savefig(f'{plot_path}/deltas_{filenames[ind]}.png', dpi = 200)
+#    plt.show() 
+    plt.clf()
 # g = {'File and Direction': file_dir_list, 'Standard Deviation (nT)': std_list}
 # df = pd.DataFrame(data = g)
 # df.to_csv(f'{csv_path}/stats.csv')
