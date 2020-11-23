04a5236107016393ee3adc04347c6df1d4bce789
rmdocherty <rmdocherty2@gmail.com>
2020-11-23 17:41:28 +0000

    Plotted compression ratio for delta as fn of # of waves

---
+++ satellite/compression_ratios/modified_comp_rat.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 23 16:55:16 2020
+
+@author: ronan
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def pretty_graph(x_label, y_label, title, fontsize): #formatting graphs
+    plt.xlabel(x_label,fontsize=fontsize)
+    plt.title(title,fontsize=fontsize)
+    plt.ylabel(y_label,fontsize=fontsize) 
+    plt.tick_params(labelsize=fontsize)
+    plt.legend()
+
+with open('d_m_1_5.txt') as f:
+    lines = [float(line.rstrip()) for line in f]
+
+labels = "xyz"
+markers = ["^", ".", "s"]
+x = []; y = []; z = [];
+data = [x,y,z]
+for i in range(0, len(lines)):
+    data[i%3].append(lines[i])
+wave_no = [i for i in range(1, len(data[0])+1)]
+for i in range(3):
+    plt.plot(wave_no, data[i], label=labels[i], marker=markers[i], ms=20,ls="-",  lw=4)
+    
+pretty_graph("Number of waves", "Compression ratio (%)", \
+             "Compression ratio of Delta encoding as a function of interference", 24)
+plt.legend(fontsize=20)
+plt.gca().set_facecolor("#fffcf5")
--- satellite/scripts/small_test.cpp
+++ satellite/scripts/small_test.cpp
@@ -9,18 +9,25 @@ int main(int argc, char* argv[]){
 	int BITS = std::stoi(argv[6]);
 	std::vector<std::string> filePaths = generateFileList(FILELIST);
 	std::string dirs[3] = {"x", "y", "z"};
+	std::vector<float> maxCompressionRatios;
 	for (int fileIndex=0; fileIndex < filePaths.size(); fileIndex++){
 		Encoder temp = Encoder(3, filePaths[fileIndex], SAMPLE_SIZE, "x", MODE, BITS);
 		Encoder* d = temp.makeEncoder(ENC_TYPE);
 		d->loadData();
 		for (int j=0; j<3; j++){
+			std::vector<float> compressionRatios;
 			d->setDirection(dirs[j]);
-			for (int i=21; i< 22; i++){
+			for (int i=3; i< 100; i++){
 				d->setBlockSize(i);
 				d->genSamples(false);
-				d->encodeData(true);
+				d->encodeData(false);
+				float compRat = d->getCompressionRatio();
+				compressionRatios.push_back(compRat);
 			}
+			float max = *std::max_element(compressionRatios.begin(), compressionRatios.end());
+			maxCompressionRatios.push_back(max);
 		}
 	}
+	csv_save(OUTFILE, maxCompressionRatios);
 	return 0;
 }
