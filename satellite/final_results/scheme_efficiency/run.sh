3a612b7a1239dfd072a273cc29e739ee00dd8cc8
Vinstagramx <vincentwong0401@gmail.com>
2020-12-08 18:10:09 +0000

    delta 1-10 + tols (flat 10, updated interference)

--- satellite/final_results/scheme_efficiency/run.sh
+++ satellite/final_results/scheme_efficiency/run.sh
@@ -1,4 +1,4 @@
-for i in 6 7 8 9
+for i in 1 2 3 4 5 6 7 8 9 10
 do 
-    ./mtf "Delta" "7000" "mean" "delta_iters_$i.txt" "interference_file_list.txt" "16" "False" "$i"
+    ./mtf "Delta" "7000" "mean" "delta_iters_$i.txt" "interference_file_list.txt" "16" "True" "$i" "delta_tols_$i.txt"
 done
